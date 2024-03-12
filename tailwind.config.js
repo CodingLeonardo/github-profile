d6656196a9a22eb2765a493cb9dc9f5f3b147c88
Leonardo Rivero <codingleonardo@gmail.com>
2024-03-12 10:38:39 -0400

    Add new color palette to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#000000",
+      gray: {
+        asphalt: "#111729",
+        anthracite: "#1D1B48",
+        slate: "#20293A",
+        pearl: "#CDD5E0",
+      },
+      blue: {
+        electric: "#3662E3",
+        sapphire: "#364153",
+        navy: "#4A5567",
+      },
+    },
     extend: {},
   },
   plugins: [],
