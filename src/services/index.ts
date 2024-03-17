291a9f15361117820faeb33c81330af74f4761a1
Leonardo Rivero <codingleonardo@gmail.com>
2024-03-17 14:34:40 -0400

    Add GitHub Pages deployment workflow and update GitHub token

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 import { Octokit } from "octokit"
-const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN as string;
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN as string;
 
 export const octokit = new Octokit({ auth: GITHUB_TOKEN });
