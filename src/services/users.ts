99ae16eb975f6cf5e15c069cf5c599e89ece9e18
Leonardo Rivero <codingleonardo@gmail.com>
2024-04-19 17:01:08 -0400

    Refactor getUser, getMultipleUsers, and searchUser functions in users.ts

--- src/services/users.ts
+++ src/services/users.ts
@@ -3,20 +3,26 @@ import { User, UsersSearch } from "../types";
 
 export const getUser = async (username: string) => {
   const { data } = await octokit.rest.users.getByUsername({ username });
-  return data
+  return data;
 };
 
 export const getMultipleUsers = async (users: UsersSearch): Promise<User[]> => {
-  const usernames = users.map((user) => user.login)
-  const promises = Promise.all(usernames.map(async (username) => {
-    const user = await getUser(username)
-    return user
-  }))
+  const usernames = users.map((user) => user.login);
+  const promises = Promise.all(
+    usernames.map(async (username) => {
+      const user = await getUser(username);
+      return user;
+    })
+  );
   const results = await promises;
-  return results
+  return results;
 };
 
 export const searchUser = async (username: string): Promise<UsersSearch> => {
-  const { data } = await octokit.rest.search.users({ q: username, order: "desc", per_page: 5});
-  return data.items
-}
+  const { data } = await octokit.rest.search.users({
+    q: username,
+    order: "desc",
+    per_page: 5,
+  });
+  return data.items;
+};
