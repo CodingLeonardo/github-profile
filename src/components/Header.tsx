e9b331804958d41d70e1ffc6ba2cfddf4901add0
Leonardo Rivero <codingleonardo@gmail.com>
2024-04-19 17:02:15 -0400

    Update header component to remove commented out code and add background image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
 import Search from "./Search";
-// import Hero from "../assets/hero-image-github-profile.png";
 
 const Header: FC = () => {
   return (
     <header className="h-60 grid justify-items-center bg-hero bg-cover bg-center">
       <Search />
-      {/* <img src={Hero} alt="Hero" className="h-60 w-full object-cover" /> */}
     </header>
   );
 };
